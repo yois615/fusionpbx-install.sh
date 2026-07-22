ea0462e6ba2e68c3f17192f8bbd07635aa1d1b4f
yois615 <ynadiv@corpit.xyz>
2026-07-22 10:16:50 -0400

    [config] Fix install of 1.11.1

--- debian/resources/config.sh
+++ debian/resources/config.sh
@@ -6,10 +6,10 @@ system_password=random                      # random or a custom value
 system_branch=5.5.7-CORPIT                  # master, 5.5.7
 
 # FreeSWITCH Settings
-switch_branch=master                        # master, stable
+switch_branch=stable                       # master, stable
 switch_source=true                          # true (source compile) or false (binary package)
 switch_package=false                        # true (binary package) or false (source compile)
-switch_version=1.10.12                      # which source code to download, only for source
+switch_version=1.11.1                      # which source code to download, only for source
 switch_tls=true                             # true or false
 switch_token=                               # Get the auth token from https://signalwire.com
                                             # Signup or Login -> Profile -> Personal Auth Token
