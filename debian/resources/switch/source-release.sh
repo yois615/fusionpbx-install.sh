ec24ec6a1bc119999676ff44f51e6adfe8fcbc07
yois615 <ynadiv@corpit.xyz>
2026-08-18 11:47:52 -0400

    libjansson-dev

--- debian/resources/switch/source-release.sh
+++ debian/resources/switch/source-release.sh
@@ -17,7 +17,7 @@ apt install -y liblua5.2-dev libtiff5-dev libperl-dev libcurl4-openssl-dev libsq
 apt install -y devscripts libspeexdsp-dev libspeex-dev libldns-dev libedit-dev libopus-dev libmemcached-dev
 apt install -y libshout3-dev libmpg123-dev libmp3lame-dev yasm nasm libsndfile1-dev libuv1-dev libvpx-dev
 apt install -y libavformat-dev libswscale-dev libvlc-dev sox libsox-fmt-all
-apt install -y libtiff5-dev
+apt install -y libtiff5-dev libjansson-dev
 apt install -y libpcre3-dev
 
 #install dependencies that depend on the operating system version
